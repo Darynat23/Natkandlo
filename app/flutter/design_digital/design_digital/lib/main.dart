b260dcf604ed1d4b0fe1eb04ad3613999a063aed
Daryan23 <daryanmejia0923@gmail.com>
2020-07-13 19:52:56 -0500

    Dos primeras pantallas

--- app/flutter/design_digital/design_digital/lib/main.dart
+++ app/flutter/design_digital/design_digital/lib/main.dart
@@ -1,117 +1,220 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+Widget Registro() {
+  return Scaffold(
+    body: Container(
+      child: Column(
+        children: [
+          titulo(),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 65.0),
+              child: Container(
+                width: 300,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                  child: Formulario(),
+                ),
+                color: Color.fromRGBO(243, 243, 243, 0.75),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+Widget titulo() {
+  return Padding(
+    padding: const EdgeInsets.only(top: 85),
+    child: Center(
+      child: Text(
+        "Registrarte",
+        style: TextStyle(fontSize: 35, fontFamily: "Georgia"),
+      ),
+    ),
+  );
+}
+
+class Formulario extends StatefulWidget {
+  Formulario({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyStatefulWidgetState();
+  }
+}
+
+class _MyStatefulWidgetState extends State<Formulario> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nombre',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Complete su nombre';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Apellidos',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Complete sus apellidos';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Correo',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Complete su correo';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nombre de usuario',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Complete su nombre de usuario';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Clave',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Complete su clave';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // Process data.
+                }
+              },
+              child: Text('Enviar'),
+            ),
+          ),
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(fontFamily: "Georgia"),
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Color(0xffFEB294)),
+        body: Column(
+          children: [
+            Container(
+              child: Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 19.0, top: 15.0),
+                      child: Text(
+                        'Nombre de usuario',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Icon(Icons.accessibility),
+                    ),
+                  ],
+                ),
+              ),
+            )
+            ,FormularioConfig(
+            )
+          ],
+        ),
+      )));
+}
+class FormularioConfig extends StatefulWidget {
+  FormularioConfig({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyStatefulWidgetStateConfig();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyStatefulWidgetStateConfig extends State<FormularioConfig> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nombre',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Complete su nombre';
+                }
+                return null;
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
