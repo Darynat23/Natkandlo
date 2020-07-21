8bb8f8367b8f22a9893a24fc85390a819bec3a91
Daryan23 <daryanmejia0923@gmail.com>
2020-07-21 18:38:47 -0500

    Prueba fallida pantalla 2

--- app/flutter/design_digital/design_digital/lib/main.dart
+++ app/flutter/design_digital/design_digital/lib/main.dart
@@ -308,27 +308,4 @@ class _MyStatefulWidgetStateConfig extends State<FormularioConfig> {
     );
   }
 }
-final _formKey = GlobalKey<FormState>();
-
-Widget build(BuildContext context) {
-  return Form(
-      key: _formKey,
-      child:
-      Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12.0),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Nombre',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Complete su nombre';
-              }
-              return null;
-            },
-          ),
-        )
-      ]));
-}
 
