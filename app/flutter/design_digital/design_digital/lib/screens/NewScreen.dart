b699552ed9c617a35d5af43c1c4f4619bd8c8ccf
rodriguezborjawilliam <rodriguezborjawilliam@gmail.com>
2020-07-25 20:50:35 -0500

    add library tap in navigation

--- app/flutter/design_digital/design_digital/lib/screens/NewScreen.dart
+++ app/flutter/design_digital/design_digital/lib/screens/NewScreen.dart
@@ -1,3 +1,4 @@
+import 'package:design_digital/widgets/HeadBar.dart';
 import 'package:design_digital/widgets/Navigation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,10 @@ class NewScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return  Scaffold(
+      appBar: HeadBar(),
+      body: Container(
+        child: Text("Hello"),
+      ),
      );
   }
 
