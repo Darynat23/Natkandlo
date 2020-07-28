d81e6fce1f02ad4e8e975f37861ded0a1a956322
rodriguezborjawilliam <rodriguezborjawilliam@gmail.com>
2020-07-27 19:08:40 -0500

    login

--- app/flutter/design_digital/design_digital/lib/screens/Auth.dart
+++ app/flutter/design_digital/design_digital/lib/screens/Auth.dart
@@ -1,10 +1,6 @@
-import 'dart:async';
-import 'dart:convert' show json;
-
+import 'package:design_digital/utils/auth.dart';
 import 'package:design_digital/widgets/HeadBar.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import "package:http/http.dart" as http;
 
 class AuthScreen extends StatelessWidget {
   @override
@@ -19,113 +15,23 @@ class AuthScreen extends StatelessWidget {
   }
 }
 
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: <String>[
-    'email',
-  ],
-);
-
 class SignIn extends StatefulWidget {
   @override
-  State createState() => SignInState();
+  _SignInState createState() => _SignInState();
 }
 
-class SignInState extends State<SignIn> {
-  GoogleSignInAccount _currentUser;
-  String _contactText;
-
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
-      setState(() {
-        _currentUser = account;
-      });
-      if (_currentUser != null) {
-
-      }
-    });
-
-
-    if (_currentUser == null) {
-//      _googleSignIn.signIn();
-    }
-
-  }
-
-
-  String _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic> connections = data['connections'];
-    final Map<String, dynamic> contact = connections?.firstWhere(
-      (dynamic contact) => contact['names'] != null,
-      orElse: () => null,
-    );
-    if (contact != null) {
-      final Map<String, dynamic> name = contact['names'].firstWhere(
-        (dynamic name) => name['displayName'] != null,
-        orElse: () => null,
-      );
-      if (name != null) {
-        return name['displayName'];
-      }
-    }
-    return null;
-  }
-
-  Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  Future<void> _handleSignOut() => _googleSignIn.disconnect();
-
-  Widget _buildBody() {
-    if (_currentUser != null) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: _currentUser,
-            ),
-            title: Text(_currentUser.displayName ?? ''),
-            subtitle: Text(_currentUser.email ?? ''),
-          ),
-          const Text("Signed in successfully."),
-          Text(_contactText ?? ''),
-          RaisedButton(
-            child: const Text('SIGN OUT'),
-            onPressed: _handleSignOut,
-          ),
-          RaisedButton(
-            child: const Text('REFRESH'),
-            onPressed: null,
-          ),
-        ],
-      );
-    } else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-
-         Container(
-
-         ),
-
-          RaisedButton(
-            child: const Text('Inicia con Google'),
-            onPressed: _handleSignIn,
-          ),
-        ],
-      );
-    }
-  }
-
+class _SignInState extends State<SignIn> {
+  bool isAuthenticated = isAuth();
   @override
   Widget build(BuildContext context) {
-    return _buildBody();
+    return Container(
+      child: (
+          RaisedButton(
+        onPressed: () {
+          AuthProvider().loginWithGoogle().then((value) => {print(value)});
+        },
+        child: Text("Iniciar con Google"),
+      )),
+    );
   }
 }
